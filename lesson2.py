90dd077dea63e874116af25c26ae5d91caf4a992
Tornikekarelidz <138922419+Tornikekarelidz@users.noreply.github.com>
2023-07-07 23:16:13 +0400

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,22 @@
+name = "Tornike"
+surname = "karelidze"
+height = 1.65
+age = 14
+favsport = "Fexburti"
+country = "Saqartvelo,Tbilisi"
+favjob = "Programireba,Editingi"
+favfood = "Xinkali,Mwvadi"
+zodiacsign = "tevzebi"
+favmusicgenre= "classic,jazz"
+favmovies = "The Godfather,Hicksaw Ridge,Bronx Tale,The Matrix, da sxva..  "
+
+
+
+
+
+
+
+
+print("chemi saxelia" + " " + name + " " + surname + " " + "me var" + " " + str(height) + " " + "da var" + " " +  country + "dan" + " " + str(age) + " " + "wlis." + " " + 
+"sayvareli sporti" + " " + favsport + "." + " " + "sayvareli saqmianobaa" + " " + favjob + "." + " " + "miyvars" + " " + favfood + "." + "chemi zodiaqoa" + " " + zodiacsign
++ "." + "fav music janria" + " " + favmusicgenre + " " + "da sayvareli filmebia" + " " + favmovies)
